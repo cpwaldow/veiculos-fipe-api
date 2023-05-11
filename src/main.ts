212a24602a0d3c891bf4a83dfa975313483258f6
Carlos Eduardo <cpwaldow@gmail.com>
2023-05-11 02:10:31 -0300

    fetch por tipo de veiculo com base na opcao

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-const marcasAPI = 'https://parallelum.com.br/fipe/api/v1/carros/marcas';
 const app = document.getElementById('app');
+const btns = document.querySelectorAll('button');
+btns?.forEach((btn: HTMLButtonElement) => {
+  btn.addEventListener('click', (event) => {
+    if (app) app.innerHTML = '';
+    const value = (event.target as Element).innerHTML;
+    fetchMarcas(value);
+  });
+});
 
 interface Marca {
   codigo: string;
@@ -14,10 +21,10 @@ const renderMarcas = (data: Array<Marca>) => {
   });
 };
 
-const fetchMarcas = async () => {
-  const response = await fetch(marcasAPI);
+const fetchMarcas = async (tipo: string) => {
+  const response = await fetch(
+    `https://parallelum.com.br/fipe/api/v1/${tipo}/marcas`,
+  );
   const data = await response.json();
   renderMarcas(data);
 };
-
-fetchMarcas();
